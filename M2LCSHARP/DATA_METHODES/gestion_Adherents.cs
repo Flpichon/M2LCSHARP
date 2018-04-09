2d34ac1af80816480fc78da32022733801551d99
Flpichon <33513602+Flpichon@users.noreply.github.com>
2018-04-09 11:51:14 +0200

    ajout classe

---
+++ M2LCSHARP/DATA_METHODES/gestion_Adherents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2LCSHARP.DATA_METHODES
+{
+    class gestion_Adherents
+    {
+        private List<DATA.adherent> liste { get; set; }
+
+        public void Liste_Adherent()
+        {
+            liste = gestion_Listes.Créer_Liste();
+        }
+
+        public void ajouter_Adherent(DATA.adherent unAdherent)
+        {
+            liste.Add(unAdherent);
+        }
+    }
+}
---
+++ M2LCSHARP/DATA_METHODES/gestion_Listes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2LCSHARP.DATA_METHODES
+{
+    class gestion_Listes
+    {
+        public static List<DATA.adherent> Créer_Liste()
+        {
+            List<DATA.adherent> Liste = new List<DATA.adherent>();
+            return Liste;
+        }
+    }
+}
