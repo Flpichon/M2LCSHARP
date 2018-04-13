16f352908b9396a5e6cfc90911d99c05adbc9fdd
Flpichon <33513602+Flpichon@users.noreply.github.com>
2018-04-13 16:17:46 +0200

    Ajouts des dossiers Vues / DATA / DATA_METHODES

---
+++ M2LCSHARP/BDD/connexion_BDD.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace M2LCSHARP.BDD
+{
+    class connexion_BDD
+    {
+        static private string connectionString;
+
+
+        public connexion_BDD()
+        {
+            ConnexionBDD();
+        }
+        static public void ConnexionBDD()
+        {
+            string server = "localhost";
+            string port = "3306";
+            string database = "m2lcsharp";
+            string uid = "root";
+            string password = "";
+            connectionString = "SERVER=" + server + ";" + "PORT=" + port + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+        }
+
+    }
+}
---
+++ M2LCSHARP/DATA_METHODES/gestion_Adherents.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;  
+using System.Threading.Tasks;
+
+namespace M2LCSHARP.DATA_METHODES
+{
+    class gestion_Adherents
+    {
+        public List<DATA.adherent> liste { get; set; }
+
+        public void Liste_Adherent()
+        {
+            liste = gestion_Listes.Créer_Liste();
+        }
+
+        public void ajouter_Adherent(DATA.adherent unAdherent)
+        {
+            liste.Add(unAdherent);
+        }
+
+        public void supprimer_Adherent(DATA.adherent unAdherent)
+        {
+            liste.Remove(unAdherent);
+        }
+
+        /*public List<DATA.adherent> Get_adherent()
+        {
+            return this.liste;
+        }*/
+    }
+}
